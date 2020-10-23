d0f18ed54ec98dc2b422a5b33f005a5cfce08872
Ruben Alpredo <alpredo.tampubolon@gmail.com>
2020-10-23 19:32:33 +0700

    Update Landpage

---
+++ packages.js
@@ -0,0 +1,65 @@
+'use strict'
+
+var office_package = [
+    { 
+        id: 1, 
+        name: 'Microsoft Office Word', 
+        duration: '4 Minggu (2 Hari/Minggu)',
+        time: 2,
+        price: 400000
+    },
+    {
+        id: 2, 
+        name: 'Microsoft Office Excel',
+        duration: '4 Minggu (3 Hari/Minggu)',
+        time: 2,
+        price: 500000
+    },
+    { 
+        id: 3, 
+        name: 'Microsoft Office Power Point', 
+        duration: '4 Minggu (2 Hari/Minggu)',
+        time: 2,
+        price: 350000
+    },
+    { 
+        id: 3, 
+        name: 'Microsoft Office Access',
+        duration: '4 Minggu (2 Hari/Minggu)',
+        time: 2,
+        price: 400000
+    },
+
+];
+
+var css_html_package = [
+    { 
+        id: 1, 
+        name: 'Pemograman HTML', 
+        duration: '4 Minggu (3 Hari/Minggu)',
+        time: 2.5,
+        price: 600000
+    },
+    { 
+        id: 1, 
+        name: 'Desain Web (CSS)', 
+        duration: '4 Minggu (3 Hari/Minggu)',
+        time: 2,
+        price: 600000
+    },
+    { 
+        id: 1, 
+        name: 'Javascript Fundamental', 
+        duration: '4 Minggu (3 Hari/Minggu)',
+        time: 2,
+        price: 750000
+    },
+    { 
+        id: 1, 
+        name: 'Database Fundamental', 
+        duration: '4 Minggu (2 Hari/Minggu)',
+        time: 2,
+        price: 650000
+    },
+];
+
