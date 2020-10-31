6fac71147229bfcf06b6ea9dbeb363eea2661266
Ruben Alpredo <alpredo.tampubolon@gmail.com>
2020-10-31 18:05:14 +0700

    Change Course

--- packages.js
+++ packages.js
@@ -5,29 +5,29 @@ var office_package = [
         id: 1, 
         name: 'Microsoft Office Word', 
         duration: '4 Minggu (2 Hari/Minggu)',
-        time: 2,
-        price: 400000
+        time: 1.5,
+        price: 600000
     },
     {
         id: 2, 
         name: 'Microsoft Office Excel',
         duration: '4 Minggu (3 Hari/Minggu)',
         time: 2,
-        price: 500000
+        price: 750000
     },
     { 
         id: 3, 
         name: 'Microsoft Office Power Point', 
         duration: '4 Minggu (2 Hari/Minggu)',
-        time: 2,
-        price: 350000
+        time: 1,
+        price: 550000
     },
     { 
         id: 4, 
         name: 'Microsoft Office Access',
         duration: '4 Minggu (2 Hari/Minggu)',
-        time: 2,
-        price: 400000
+        time: 1,
+        price: 550000
     },
 
 ];
@@ -37,54 +37,54 @@ var css_html_package = [
         id: 1, 
         name: 'Pemograman HTML', 
         duration: '4 Minggu (3 Hari/Minggu)',
-        time: 2.5,
-        price: 600000
+        time: 1.5,
+        price: 780000
     },
     { 
         id: 2, 
         name: 'Desain Web (CSS)', 
         duration: '4 Minggu (3 Hari/Minggu)',
-        time: 2,
-        price: 600000
+        time: 1.5,
+        price: 780000
     },
     { 
         id: 3, 
         name: 'Javascript Fundamental', 
         duration: '4 Minggu (3 Hari/Minggu)',
-        time: 2,
-        price: 750000
+        time: 1,
+        price: 650000
     },
     { 
         id: 4, 
         name: 'Database Fundamental', 
         duration: '4 Minggu (2 Hari/Minggu)',
-        time: 2,
-        price: 650000
+        time: 1,
+        price: 550000
     },
 ];
 
 
-var css_html_package = [
+var web_application = [
     { 
         id: 1, 
         name: 'Aplikasi Web I (Laravel, JQuery, HTML dan CSS / Integrasi Admin Template)', 
         duration: '5 Minggu (3 Hari/Minggu)',
-        time: 2.5,
-        price: 2700000
+        time: 1.5,
+        price: 2725000
     },
     { 
         id: 2, 
         name: 'Aplikasi Web II (Express JS, HTML dan CSS / Custom Web Template)', 
         duration: '5 Minggu (3 Hari/Minggu)',
-        time: 2.5,
-        price: 3000000
+        time: 1.5,
+        price: 2725000
     },
     { 
         id: 3, 
         name: 'Node JS/Express JS Rest API', 
         duration: '4 Minggu (2 Hari/Minggu)',
-        time: 2.5,
-        price: 3000000
+        time: 1.5,
+        price: 2500000
     },
 ];
 
